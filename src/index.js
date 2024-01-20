3485e5efa9abec7a5e65c582b31ce30f7102acee
This is XiaoDeng <1744793737@qq.com>
2024-01-20 21:53:47 +0800

    完成基本功能

--- src/index.js
+++ src/index.js
@@ -8,8 +8,35 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+function getHtmlContent(cave_id, context, sender) {
+	return `<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>XDbot2 回声洞</title>
+	</head>
+	<body style="text-align: center;">
+		<p>回声洞——（${cave_id}）</p>
+		<p>${context}</p>
+		<p>——${sender}</p>
+	</body>
+</html>`;
+}
+
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		let data = await (await fetch("https://caveapi.itcdt.top/random")).json();
+		let content = data["content"];
+		for (var key in data["images"]) {
+			content = content.replace(
+				`[[Img:${key}]]]`,
+				`<img src="data:image/png;base64,${data["images"][key]}" />`
+			);
+		}
+		return new Response(
+      getHtmlContent(data["id"], content, data["sender"]), {
+        headers: { 'Content-Type': 'text/html' }
+      }
+    );
 	},
 };
